846982d5124eec49ff4682579287124eed42ec4a
mygithub2211 <phattran13062002@gmail.com>
2024-07-29 00:52:07 -0400

    Commit

--- backEnd/index.js
+++ backEnd/index.js
@@ -4,18 +4,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://music-web-app-client.vercel.app', // Your frontend URL
+  methods: ['GET', 'POST'], // Allowed methods
+  allowedHeaders: ['Content-Type'], // Allowed headers
+}));
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-//mongoose.connect("mongodb+srv://new_user:Z0v4rz23993@mycluster.xazafns.mongodb.net/Ptran_Database", {});
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {});
+mongoose.connect(process.env.MONGODB_URI);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -27,7 +30,7 @@ db.once("open", () => {
 const userSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
-  email: String,
+  email: { type: String, unique: true },
   password: String,
 });
 
@@ -36,8 +39,12 @@ const User = mongoose.model("User", userSchema);
 // Register route
 app.post("/register", async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
-  const newUser = new User({ firstName, lastName, email, password });
+  
   try {
+    // Hash password before saving
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const newUser = new User({ firstName, lastName, email, password: hashedPassword });
     await newUser.save();
     res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
@@ -48,10 +55,13 @@ app.post("/register", async (req, res) => {
 // Login route
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
+  
   try {
-    const user = await User.findOne({ email, password });
-    if (user) {
-      res.json({ success: true });
+    const user = await User.findOne({ email });
+    if (user && await bcrypt.compare(password, user.password)) {
+      // Generate JWT token
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+      res.json({ success: true, token });
     } else {
       res.json({ success: false });
     }
@@ -112,7 +122,6 @@ app.get("/my_songs", async (req, res) => {
 // Route to add song to my_songs collection
 app.post("/my_songs", async (req, res) => {
   const { title, subtitle, src, imgSrc } = req.body;
-  const newSong = new myList({ title, subtitle, src, imgSrc });
   try {
     await newSong.save();
     res.status(201).json({ message: "Song added to my_songs successfully" });
